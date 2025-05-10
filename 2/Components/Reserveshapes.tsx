dd0a4d0238456ac3a8205ea76c60091d3f80b9f6
amine502 <78903844+amine502@users.noreply.github.com>
2025-05-10 14:42:27 +0100

    adding on load transition

--- 2/Components/Reserveshapes.tsx
+++ 2/Components/Reserveshapes.tsx
@@ -27,7 +27,18 @@ function Reserveshapes() {
   const [boxesReady, setBoxesReady] = useState(false);
 
   useEffect(() => {
-    // Set a flag after the first render
+    gsap.fromTo(".etages",{
+      yPercent:200,
+      'webkitFilter': 'blur(25px)',
+       opacity:0,
+
+  },{
+      yPercent:0,
+      'webkitFilter': 'blur(0px)',
+      opacity:1,
+      onComplete:()=>{    gsap.set(".etages", {translate:""});}
+  })
+
     setBoxesReady(true);
   }, []);
   const [nextitems,setnextitems]=useState(0);
@@ -87,9 +98,18 @@ gsap.to(".notselecteddown",{
    ease:"power4.out"
 
 
-                })}
+                })
+
+
+
+
+              }
+
+
 
   },[selected])
+
+
   const svgobj=(<></>)
   const [etages,setetages] = useState<Etage[]>([
     { id:0,
@@ -422,16 +442,12 @@ gsap.to(".notselecteddown",{
             <button className={selected.isseleced && selected.id === index ? "self-end p-2 rounded-lg cursor-pointer" : "hidden"} style={{ backgroundColor: editMode ? "blue" : "green" }} onClick={() => {
                 setEditMode(prev => !prev);
             } }>ADD/EDIT</button>
-            <button className={selected.isseleced && selected.id === index ? "self-end p-2 rounded-lg cursor-pointer" : "hidden"} style={{ backgroundColor: "red" }} onClick={() => {
-
-                window.location.reload();
-            } }>Return</button>
 
         </div>
     </div>);
   return (
     <div>
-    <div className=" flex flex-col items-center justify-center overflow-hidden  w-full h-full" >
+    <div className=" flex flex-col items-center justify-center overflow-hidden etages opacity-0 w-full h-full" >
       {Showedetages[nextitems].map((item, index) => (<React.Fragment    key={index} >
 { newLocal(item,index," absolute transition-transform duration-300 hover:translate-y-[-10px] upper ") }
 
@@ -440,7 +456,7 @@ gsap.to(".notselecteddown",{
 {Showedetages[nextitems].map((itemnext,indexnext)=><React.Fragment key={indexnext} >{indexnext>2? ""
 :newLocal(itemnext,indexnext,"blur-[20px] opacity-0.2 nexto ")}</React.Fragment> )}</div>
 <div className="absolute bottom-0 right-0 mb-4 flex flex-col gap-2">
-{ nextitems!==0&& <button  onClick={()=>{setTimeout(()=>setnextitems(prev=>prev-1),2000)
+{ (nextitems!==0&&!selected.isseleced)&& <button  onClick={()=>{setTimeout(()=>setnextitems(prev=>prev-1),2000)
 
 const next = gsap.utils.toArray(".nexto");
 const upper = gsap.utils.toArray(".upper")
@@ -498,57 +514,20 @@ if(!boxesReady)return;
 
 
  </button>}
-{ nextitems!==0&& <button  onClick={()=>{setTimeout(()=>setnextitems(prev=>prev-1),2000)
+{ selected.isseleced&& <button  onClick={()=>{
+   if(selected.isseleced) {
 
-const next = gsap.utils.toArray(".nexto");
-const upper = gsap.utils.toArray(".upper")
-if(!boxesReady)return;
- gsap.to(upper,{
-     yPercent:-65,
-     duration:1,
-     stagger:0.3,
-     opacity:0,
-     onComplete:()=>{
-         gsap.fromTo(upper,{
-             yPercent:200,
-
-             'webkitFilter': 'blur(25px)',
-             opacity:0,
-
-         },{
-             yPercent:0,
-             'webkitFilter': 'blur(0px)',
-             opacity:1,
-             onComplete:()=>{    gsap.set(upper, {translate:""});}
-         })
-     }
- })
-
-
- gsap.to(next,{
-   opacity:0,
-   delay:1,
-   onComplete:()=>{
-     gsap.fromTo(next,{
-         'webkitFilter': 'blur(0px)',
-         opacity:0
-
-     },{
-
-         'webkitFilter': 'blur(20px)',
-         delay:1,
-         scale:1.1,
-         opacity:1,
-
-     })
- }
-
+gsap.to(".etages",{
+  opacity:0,
 })
+
+ window.location.reload()
+  }
 
 }} className="
   cursor-pointer flex flex-col gap-2 items-center
   justify-center text-2xl  transition-opacity duration-300 hover:bg-[#ffe2e256] p-4  ">
- <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+ <svg xmlns="http://www.w3.org/2000/svg" className="w-4" viewBox="0 0 576 512">
   <path fill="#ffffff" d="M264.5 5.2c14.9-6.9 32.1-6.9 47 0l218.6 101c8.5 3.9
   13.9 12.4 13.9 21.8s-5.4 17.9-13.9 21.8l-218.6 101c-14.9 6.9-32.1 6.9-47 0L45.9
   149.8C37.4 145.8 32 137.3 32 128s5.4-17.9 13.9-21.8L264.5 5.2zM476.9 209.6l53.2
@@ -561,7 +540,7 @@ if(!boxesReady)return;
 
  </button>}
 
-    { nextitems!=Showedetages.length-1&& <button onClick={()=>{setTimeout(()=>setnextitems(prev=>prev+1),2000)
+    { ((nextitems!=Showedetages.length-1)&&!selected.isseleced)&& <button onClick={()=>{setTimeout(()=>setnextitems(prev=>prev+1),2000)
 
    const next = gsap.utils.toArray(".nexto");
    const upper = gsap.utils.toArray(".upper")
