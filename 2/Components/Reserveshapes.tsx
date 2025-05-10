f528a9ba7be22ed4168cfa6a74496651d804af97
amine502 <78903844+amine502@users.noreply.github.com>
2025-05-10 14:22:18 +0100

    removing aissa xd

--- 2/Components/Reserveshapes.tsx
+++ 2/Components/Reserveshapes.tsx
@@ -498,6 +498,69 @@ if(!boxesReady)return;
 
 
  </button>}
+{ nextitems!==0&& <button  onClick={()=>{setTimeout(()=>setnextitems(prev=>prev-1),2000)
+
+const next = gsap.utils.toArray(".nexto");
+const upper = gsap.utils.toArray(".upper")
+if(!boxesReady)return;
+ gsap.to(upper,{
+     yPercent:-65,
+     duration:1,
+     stagger:0.3,
+     opacity:0,
+     onComplete:()=>{
+         gsap.fromTo(upper,{
+             yPercent:200,
+
+             'webkitFilter': 'blur(25px)',
+             opacity:0,
+
+         },{
+             yPercent:0,
+             'webkitFilter': 'blur(0px)',
+             opacity:1,
+             onComplete:()=>{    gsap.set(upper, {translate:""});}
+         })
+     }
+ })
+
+
+ gsap.to(next,{
+   opacity:0,
+   delay:1,
+   onComplete:()=>{
+     gsap.fromTo(next,{
+         'webkitFilter': 'blur(0px)',
+         opacity:0
+
+     },{
+
+         'webkitFilter': 'blur(20px)',
+         delay:1,
+         scale:1.1,
+         opacity:1,
+
+     })
+ }
+
+})
+
+}} className="
+  cursor-pointer flex flex-col gap-2 items-center
+  justify-center text-2xl  transition-opacity duration-300 hover:bg-[#ffe2e256] p-4  ">
+ <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
+  <path fill="#ffffff" d="M264.5 5.2c14.9-6.9 32.1-6.9 47 0l218.6 101c8.5 3.9
+  13.9 12.4 13.9 21.8s-5.4 17.9-13.9 21.8l-218.6 101c-14.9 6.9-32.1 6.9-47 0L45.9
+  149.8C37.4 145.8 32 137.3 32 128s5.4-17.9 13.9-21.8L264.5 5.2zM476.9 209.6l53.2
+  24.6c8.5 3.9 13.9 12.4 13.9 21.8s-5.4 17.9-13.9 21.8l-218.6 101c-14.9 6.9-32.1 6.9-47 0L45.9 277.8C37.4 273.8
+  32 265.3 32 256s5.4-17.9 13.9-21.8l53.2-24.6 152 70.2c23.4 10.8 50.4 10.8 73.8 0l152-70.2zm-152 198.2l152-70.2
+  53.2 24.6c8.5 3.9 13.9 12.4 13.9 21.8s-5.4 17.9-13.9 21.8l-218.6 101c-14.9 6.9-32.1 6.9-47 0L45.9 405.8C37.4 401.8 32 393.3 32 384s5.4-17.9
+  13.9-21.8l53.2-24.6 152 70.2c23.4 10.8 50.4 10.8 73.8 0z"/></svg>
+
+
+
+ </button>}
+
     { nextitems!=Showedetages.length-1&& <button onClick={()=>{setTimeout(()=>setnextitems(prev=>prev+1),2000)
 
    const next = gsap.utils.toArray(".nexto");
